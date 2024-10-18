5d876497ad8b75da34dccb78f4ee30c8cbf351ea
91704 <91704@SANDEEP>
2024-10-18 13:51:26 +0530

    first commit

--- IPL_Team_JDBC/src/com/tka/client/IPLClient.java
+++ IPL_Team_JDBC/src/com/tka/client/IPLClient.java
@@ -6,6 +6,8 @@ import com.tka.controller.IPLcontroller;
 import com.tka.utility.Player;
 
 public class IPLClient {
+	
+	
 	public static void main(String[] args) {
 		System.out.println("we are in IPl client");
 		IPLcontroller controller=null;
